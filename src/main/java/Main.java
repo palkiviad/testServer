8af6cc2c5442887d807ec46243d6907bbc9b1ab8
alkiviad <ppopov_2010@mail.ru>
2018-04-12 00:21:51 +0300

    minor changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,8 @@ import org.eclipse.jetty.servlet.ServletHolder;
 import servlets.MyCustomServlet;
 import servlets.ServletsContainer;
 
+import javax.servlet.MultipartConfigElement;
+
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -11,6 +13,7 @@ public class Main {
         ServletContextHandler handler = new ServletContextHandler(ServletContextHandler.SESSIONS);
         for (MyCustomServlet servlet : ServletsContainer.getList()) {
             ServletHolder holder = new ServletHolder(servlet);
+            holder.getRegistration().setMultipartConfig(new MultipartConfigElement(""));
             handler.addServlet(holder, servlet.getPath());
         }
 
--- src/main/java/servlets/WelcomeServlet.java
+++ src/main/java/servlets/WelcomeServlet.java
@@ -3,6 +3,7 @@ package servlets;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 import java.io.IOException;
 
 /**
@@ -12,6 +13,15 @@ public class WelcomeServlet extends MyCustomServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.getWriter().println("HELLo WORLD!");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String partsValue = "";
+        for (Part part :req.getParts()) {
+            partsValue += part.getName();
+        }
+        resp.getWriter().println("it's a response from post "+  partsValue);
     }
 
     @Override
